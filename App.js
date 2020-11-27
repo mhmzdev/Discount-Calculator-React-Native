80a5c918586276d00efebaa8a86fc519f1509716
m-hamzashakeel <hamza.6.shakeel@gmail.com>
2020-11-27 09:11:10 +0500

    Done with Task # 2

--- App.js
+++ App.js
@@ -4,20 +4,34 @@ import {
   Text,
   StyleSheet,
   TouchableOpacity,
+  TextInput,
 } from 'react-native';
 
 
 const App = () => {
 
   return (
-    <View>
-      <Text>Hello Jee</Text>
+    <View style={styles.mainView}>
+      <TextInput style={styles.textFields} placeholder={"Original Price"} />
+      <View style={{ paddingTop: 10 }} />
+      <TextInput style={styles.textFields} placeholder={"Discount %"} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  mainView: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  },
+  textFields: {
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 10
+  }
 });
 
 export default App;
