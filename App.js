2d5fcbf31f7ce210f057af2aabf1855bf728b68f
m-hamzashakeel <hamza.6.shakeel@gmail.com>
2020-11-27 09:40:56 +0500

    Done with Task # 3 - Calculations

--- App.js
+++ App.js
@@ -10,11 +10,37 @@ import {
 
 const App = () => {
 
+  const [originalPrice, setOriginalPrice] = useState(0);
+  const [discountPrc, setDicountPrc] = useState(0);
+  const [savedAmount, setSavedAmount] = useState(0);
+  const [finalPrice, setFinalPrice] = useState(0);
+
+  calculateDiscount = () => {
+    var saved = (originalPrice * discountPrc) / 100;
+    var final_Price = originalPrice - saved;
+    setSavedAmount(saved);
+    setFinalPrice(final_Price);
+  }
+
   return (
     <View style={styles.mainView}>
-      <TextInput style={styles.textFields} placeholder={"Original Price"} />
+      <TextInput keyboardType={"number-pad"} onChangeText={(orgPrice) => setOriginalPrice(orgPrice)} style={styles.textFields} placeholder={"Original Price"} />
       <View style={{ paddingTop: 10 }} />
-      <TextInput style={styles.textFields} placeholder={"Discount %"} />
+      <TextInput keyboardType={"number-pad"} onChangeText={(discountPercentage) => setDicountPrc(discountPercentage)} style={styles.textFields} placeholder={"Discount %"} />
+      <View style={{ paddingTop: 10 }} />
+      <TouchableOpacity onPress={() => calculateDiscount()} style={styles.calcBtn}>
+        <Text style={styles.calcBtnText}>Calculate</Text>
+      </TouchableOpacity>
+      <View style={{ paddingTop: 50 }} />
+      <View style={{ flexDirection: 'row' }}>
+        <Text style={styles.resultText}>Final Price :</Text>
+        <Text style={styles.finalPriceText}> Rs {finalPrice}</Text>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <Text style={styles.resultText}>You Saved :</Text>
+        <Text style={styles.finalPriceText}> Rs {savedAmount}</Text>
+      </View>
+
     </View>
   );
 }
@@ -26,11 +52,32 @@ const styles = StyleSheet.create({
     flex: 1
   },
   textFields: {
-    height: 40,
-    width: 200,
+    height: 50,
+    width: 280,
     borderColor: 'gray',
     borderWidth: 1,
-    paddingLeft: 10
+    paddingLeft: 10,
+    fontSize: 18,
+    borderRadius: 10,
+  },
+  calcBtn: {
+    height: 50,
+    width: 200,
+    backgroundColor: '#388E3C',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  calcBtnText: {
+    fontSize: 24,
+    color: 'white'
+  },
+  resultText: {
+    fontSize: 20,
+  },
+  finalPriceText: {
+    fontSize: 20,
+    fontWeight: "bold"
   }
 });
 
