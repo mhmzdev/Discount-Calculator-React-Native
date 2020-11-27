899c00fb8593a42b45a3658979577568d00410f0
m-hamzashakeel <hamza.6.shakeel@gmail.com>
2020-11-27 08:58:20 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <View>
+      <Text>Hello Jee</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
